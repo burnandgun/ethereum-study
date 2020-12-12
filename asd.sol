e4adcae386538fb3d62fd50701f65b9f77695d38
burnandgun <dujunjieyx@163.com>
2020-12-11 22:24:58 -0800

    personal chain

--- asd.sol
+++ asd.sol
@@ -1,8 +1,15 @@
 pragma solidity ^0.4.0;
 
 contract asd {
+
+
+
+    address public d ;
+
     function asd(){
         uint24 a = 1;
-
+        bool b = false;
+        var c = true;
+        owner.balance;
     }
 }
